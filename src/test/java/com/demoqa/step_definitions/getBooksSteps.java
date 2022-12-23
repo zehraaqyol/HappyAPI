4a00a903a770d8f6c898b4e0b9a7e27f188170d6
zehraaqyol <zehraaqyol@gmail.com>
2022-12-23 18:06:53 +0100

    add demoqa/bookstore verify first book info

--- src/test/java/com/demoqa/step_definitions/getBooksSteps.java
+++ src/test/java/com/demoqa/step_definitions/getBooksSteps.java
@@ -14,6 +14,7 @@ import java.util.List;
 public class getBooksSteps {
 
     Response response;
+    JsonPath jsonPath;
 
     @When("User sends GET request to receive all books info")
     public void user_sends_get_request_to_receive_all_books_info() {
@@ -23,7 +24,7 @@ public class getBooksSteps {
     @Then("Verifies that we have {int} books in response")
     public void verifies_that_we_have_books_in_response(int amount) {
 
-        JsonPath jsonPath = response.jsonPath();
+        jsonPath = response.jsonPath();
 
         List<Object> books = jsonPath.getList("books");
 
@@ -32,4 +33,53 @@ public class getBooksSteps {
         Assert.assertEquals(amount,books.size());
     }
 
+    @When("User sends GET request to receive first book info")
+    public void userSendsGETRequestToReceiveFirstBookInfo() {
+        response=given().queryParam("ISBN","9781449337711")
+                .accept(ContentType.JSON).get("/BookStore/v1/Book");
+        //response.prettyPrint();
+    }
+//    @Then("Verify that we have correct info about fisrt book which we selected")
+//    public void verify_that_we_have_correct_info_about_fisrt_book_which_we_selected() {
+//      jsonPath=response.jsonPath();
+//        String title= jsonPath.getString("title");
+//        System.out.println("title = " + title);
+//
+//        String subTitle=jsonPath.getString("subTitle");
+//        System.out.println("subTitle = " + subTitle);
+//
+//        String author=jsonPath.getString("author");
+//        System.out.println("author = " + author);
+//
+//        String publish_date=jsonPath.getString("publish_date");
+//        System.out.println("publish_date = " + publish_date);
+//
+//        String publisher=jsonPath.getString("publisher");
+//        System.out.println("publisher = " + publisher);
+//
+//        String pages=jsonPath.getString("pages");
+//        System.out.println("pages = " + pages);
+//
+//        String description=jsonPath.getString("description");
+//        System.out.println("description = " + description);
+//
+//        String website=jsonPath.getString("website");
+//        System.out.println("website = " + website);
+//
+//        Assert.assertEquals("Designing Evolvable Web APIs with ASP.NET",title);
+//        Assert.assertEquals("238",pages);
+//        Assert.assertEquals("Glenn Block et al.",author);
+//    }
+
+    @Then("Verify that we have correct info about fisrt book which we selected {string} {string}")
+    public void verifyThatWeHaveCorrectInfoAboutFisrtBookWhichWeSelected(String key, String value) {
+        jsonPath=response.jsonPath();
+        value= jsonPath.getString(key);
+        System.out.println(key+" = "+value);
+
+        Assert.assertEquals(jsonPath.getString(key),value);
+
+
+
+    }
 }
--- src/test/resources/features/getAllBooks.feature
+++ src/test/resources/features/getAllBooks.feature
@@ -3,3 +3,20 @@ Feature: Get All Books Info
   Scenario: Verify the number of books
     When User sends GET request to receive all books info
     Then Verifies that we have 8 books in response
+
+  @book
+  Scenario Outline: Verify response body info of the first book
+
+    When User sends GET request to receive first book info
+    Then Verify that we have correct info about fisrt book which we selected "<key>" "<value>"
+    Examples:
+      | key          | value                                                                     |
+      | isbn         | 9781449337711                                                             |
+      | title        | Designing Evolvable Web APIs with ASP.NET                                 |
+      | subTitle     | Harnessing the Power of the Web                                           |
+      | author       | Glenn Block et al.                                                        |
+      | publish_date | 2020-06-04T09:12:43.000Z                                                  |
+      | publisher    | O'Reilly Media                                                            |
+      | pages        | 238                                                                       |
+      | description  | Design and build Web APIs for a broad range of clients—including browsers and mobile devices—that can adapt to change over time. This practical, hands-on guide takes you through the theory and tools you need to build evolvable HTTP services with Microsoft|
+      | website      | http://chimera.labs.oreilly.com/books/1234000001708/index.html            |

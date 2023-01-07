5464f3718f003df82f5573ddc1d9e03106edfca5
zehraaqyol <zehraaqyol@gmail.com>
2023-01-07 21:20:30 +0100

    add BookStore api verifies okey

--- src/test/java/com/demoqa/step_definitions/GetUserInfoSteps.java
+++ src/test/java/com/demoqa/step_definitions/GetUserInfoSteps.java
@@ -9,11 +9,14 @@ import io.cucumber.java.en.When;
 import io.restassured.http.ContentType;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
+import org.junit.Assert;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 import static io.restassured.RestAssured.given;
+import static org.junit.Assert.assertEquals;
 
 public class GetUserInfoSteps {
     Response response;
@@ -23,6 +26,9 @@ public class GetUserInfoSteps {
     String responseUserID;
     List<Map<String,Object>> books;
     String responseUserName;
+    List<String>allBookISBN_List;
+    List<String>userBooksISBN_List;
+
 
     ResponseApiPage responseApiPage= new ResponseApiPage();
 
@@ -35,14 +41,19 @@ public class GetUserInfoSteps {
                .and().body(responseApiPage.getRequestBody())
                .when().post("/"+ ConfigurationReader.getProperty("apiGenerateToken"));
         JsonPath jsonPath= responseToken.jsonPath();
-        token= "Bearer"+jsonPath.getString("token");
+        token= "Bearer "+jsonPath.getString("token");
         System.out.println("token = " + token);
     }
     @When("user sends GET request to receive user information")
     public void user_sends_get_request_to_receive_user_information() {
+        //IllegalStateException: Cannot parse object because no supported Content-Type was specified in response.
+        // Content-Type was 'text/html; charset=UTF-8'. ne demek???
         String userID= BookStoreApiUtils.readFromFile();
         response= given().accept(ContentType.JSON)
+                //.and().contentType(ContentType.JSON)
                 .and().header("Authorization",token)
+               // .and().header("ContentType","application/json; charset=UTF-8")
+              // .and().contentType("application/json; charset=utf-8")
                 .when().get("/"+userID);
         response.prettyPrint();
     }
@@ -56,12 +67,19 @@ public class GetUserInfoSteps {
     }
     @When("user sends GET request to receive all books information")
     public void user_sends_get_request_to_receive_all_books_information() {
-
-
+      Response responseBooks=given().accept(ContentType.JSON)
+              .and().get("/"+ConfigurationReader.getProperty("apiAddBook"));
+      JsonPath jsonPath= responseBooks.jsonPath();
+      allBookISBN_List= new ArrayList<>();
+      allBookISBN_List=jsonPath.getList("books.isbn");
     }
     @Then("Verifies status code, username and books information")
     public void verifies_status_code_username_and_books_information() {
-
+        assertEquals(200,responseStatusCode);
+        assertEquals(requestUserName,responseUserName);
+        userBooksISBN_List= new ArrayList<>();
+        JsonPath jsonPath= response.jsonPath();
+        userBooksISBN_List=jsonPath.getList("books.isbn");
+        Assert.assertTrue(allBookISBN_List.containsAll(userBooksISBN_List));
     }
-
 }
